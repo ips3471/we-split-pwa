794321d47a3d4d6be019d9bcc6fa2d74298e5c6d
han <ips3471@gmail.com>
2023-10-31 17:50:42 +0900

    setup testing environment

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+
+type Todo = {
+	userId: number;
+	id: number;
+	title: string;
+	completed: boolean;
+};
+
+function App() {
+	return (
+		<>
+			<div>hello world</div>
+		</>
+	);
+}
+
+export default App;
