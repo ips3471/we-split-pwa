4182bc595b9a5c753b010d67d26ba143c5950f60
han <ips3471@gmail.com>
2023-11-16 19:27:54 +0900

    refactoring pages container

--- src/components/pages-container.tsx
+++ src/components/pages-container.tsx
@@ -7,34 +7,32 @@ type Props = {
 
 function PagesContainer({ pageItems }: Props) {
 	const [position, setPosition] = useState(0);
-	const indicatorContainerRef = useRef<HTMLDivElement>(null);
-	const pageScrollerRef = useRef<HTMLDivElement>(null);
+	const scrollerRef = useRef<HTMLDivElement>(null);
 
 	const handleScroll = (e: React.UIEvent<HTMLElement>) => {
-		const { clientWidth, scrollLeft, scrollWidth } = e.currentTarget;
-
-		const percentage = Math.ceil(
-			(scrollLeft / (scrollWidth - clientWidth)) * 100,
-		);
-		setPosition(percentage);
+		const { clientWidth, scrollLeft } = e.currentTarget;
+		setPosition(scrollLeft / clientWidth);
 	};
 
-	const handleMovePage = (pageId: string) => {
+	const handleMovePage = (pageId: string, index: number) => {
 		const ref = document.querySelector('#' + pageId);
 
-		pageScrollerRef.current?.scrollTo({
-			left: ref?.getBoundingClientRect().left,
-			behavior: 'smooth',
-		});
+		const screenWidth = ref?.getClientRects()[0].width;
+
+		screenWidth &&
+			scrollerRef.current?.scrollTo({
+				left: screenWidth * index,
+				behavior: 'smooth',
+			});
 	};
 
 	return (
 		<>
-			<div className='bg-[#f39d11] py-1' ref={indicatorContainerRef}>
+			<div className='bg-[#f39d11] py-1'>
 				<div className='relative flex mx-14'>
-					{pageItems.map(item => (
+					{pageItems.map((item, index) => (
 						<button
-							onClick={() => handleMovePage(item.id)}
+							onClick={() => handleMovePage(item.id, index)}
 							key={item.id}
 							className='z-10 outline-none text-white w-full'
 						>
@@ -43,15 +41,18 @@ function PagesContainer({ pageItems }: Props) {
 					))}
 
 					<div
-						style={{ transform: `translate(${position}%)` }}
+						style={{
+							transform: `translate(${position * 100}%)`,
+							width: `${100 / pageItems.length}%`,
+						}}
 						className={`absolute left-0 w-1/2 h-full bg-orange-200/30 z-10  rounded-full`}
 					></div>
 				</div>
 			</div>
 
 			<div
+				ref={scrollerRef}
 				onScroll={handleScroll}
-				ref={pageScrollerRef}
 				className='relative flex flex-1 h-full overflow-x-scroll snap-mandatory snap-x'
 			>
 				{pageItems.map(item => (
